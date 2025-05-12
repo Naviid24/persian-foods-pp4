abc6916f9a7ba0284983b1a5626d1692d9db1cfb
navid <169146311+Naviid2024@users.noreply.github.com>
2025-05-12 12:35:35 +0100

    Remove a class from cloding tag in post detail template

---
+++ env.py
@@ -0,0 +1,20 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",
+    (
+        "postgresql://neondb_owner:"
+        "npg_WxMUFC0Er3QX@ep-proud-silence-a2mx0uoh."
+        "eu-central-1.aws.neon.tech/both_payer_glove_69083"
+    ),
+)
+
+os.environ.setdefault("SECRET_KEY", "MY_SECRET_KEY_ESFAHAN")
+
+os.environ.setdefault(
+    "CLOUDINARY_URL",
+    (
+        "cloudinary://486424487844726:"
+        "tiEYEau_N9mv4NSHK_FQzHZoXh8@dtnfhjoal"
+    ),
+)
